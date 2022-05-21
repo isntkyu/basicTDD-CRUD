2e21a0125754db7e124532e20fa5a3066ba8b2bc
이준규 <jkjkjk5710@gmail.com>
2022-05-21 18:59:22 +0900

    error handle test

--- routes.js
+++ routes.js
@@ -2,20 +2,5 @@ const express = require("express");
 const router = express.Router();
 const productController = require("./controller/products");
 
-const mongoose = require("mongoose");
-
-mongoose
-  .connect("mongodb://localhost:27017", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("mongo db connected..");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-router.get("/", productController.hello);
-
+router.post("/", productController.createProduct);
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const productRoutes = require("./routes");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("mongo db connected..");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const PORT = 5000;
 
