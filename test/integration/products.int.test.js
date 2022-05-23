a19f70efd62b760cbb51ed742553a2bb64a2b6c9
isntkyu <jkjkjk5710@gmail.com>
2022-05-23 22:46:18 +0900

    getproduct by id ) unit, integration TEST

--- test/integration/products.int.test.js
+++ test/integration/products.int.test.js
@@ -23,11 +23,26 @@ it("should return 500 on POST /api/products", async () => {
   });
 });
 
+let firstProduct;
 it("GET /api/products", async () => {
   const response = await request(app).get("/api/products");
   expect(response.statusCode).toBe(200);
   expect(Array.isArray(response.body)).toBeTruthy();
   expect(response.body[0].name).toBeDefined();
   expect(response.body[0].description).toBeDefined();
-  // expect(response.body[0].price).toBeDefined();
+  firstProduct = response.body[0];
+});
+
+it("GET /api/product/:productId", async () => {
+  const response = await request(app).get(`/api/products/${firstProduct._id}`);
+  expect(response.statusCode).toBe(200);
+  expect(response.body.name).toBe(firstProduct.name);
+  expect(response.body.description).toBe(firstProduct.description);
+});
+
+it("GET id doesn't exist /api/product/:productId", async () => {
+  const response = await request(app).get(
+    `/api/products/628b5abea28f5406fe11fd33`
+  );
+  expect(response.statusCode).toBe(404);
 });
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -6,7 +6,9 @@ const allProducts = require("../data/all-products.json");
 
 productModel.create = jest.fn();
 productModel.find = jest.fn();
+productModel.findById = jest.fn();
 
+const productId = "628b5abea28f5406fe11fd30";
 let req, res, next;
 beforeEach(() => {
   req = httpMocks.createRequest();
@@ -76,6 +78,41 @@ describe("Product controller GET", () => {
     const rejectedPromise = Promise.reject(errorMessage);
     productModel.find.mockReturnValue(rejectedPromise);
     await productController.getProducts(req, res, next);
-    expect(next).toBeCalledWith(errorMessage);
+    expect(next).toHaveBeenCalledWith(errorMessage);
+  });
+});
+
+describe("Product Controller GetById", () => {
+  it("should have a getProductById", async () => {
+    expect(typeof productController.getProductById).toBe("function");
+  });
+
+  it("should call Product findById", async () => {
+    req.params.productId = productId;
+    await productController.getProductById(req, res, next);
+    expect(productModel.findById).toBeCalledWith(productId);
+  });
+
+  it("should return json body and response code 200", async () => {
+    productModel.findById.mockReturnValue(newProduct);
+    await productController.getProductById(req, res, next);
+    expect(res.statusCode).toBe(200);
+    expect(res._getJSONData()).toStrictEqual(newProduct);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should return 404 when item doesn't exist", async () => {
+    productModel.findById.mockReturnValue(null);
+    await productController.getProductById(req, res, next);
+    expect(res.statusCode).toBe(404);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should handle errors", async () => {
+    const errorMessage = { message: "Error finding product data" };
+    const rejectedPromise = Promise.reject(errorMessage);
+    productModel.findById.mockReturnValue(rejectedPromise);
+    await productController.getProductById(req, res, next);
+    expect(next).toHaveBeenCalledWith(errorMessage);
   });
 });
